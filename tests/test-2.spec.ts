611f94952807b72693b04aed604a97280dba738b
Gaurav-Shinde <gaurav.shinde@gmail.com>
2026-01-31 19:54:49 +0530

    init

---
+++ tests/test-2.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+    await page.goto('https://www.walmart.com/');
+    await page.locator('iframe[src="about:blank"]').contentFrame().getByRole('button', { name: 'Press & Hold Human Challenge' }).click();
+    await page.locator('iframe[src="about:blank"]').contentFrame().getByRole('button', { name: 'Press & Hold Human Challenge' }).click();
+});
